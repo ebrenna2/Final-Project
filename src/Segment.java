dc1e1953908d73907a958f50c27b2121e1f09bed
Rogers <brogers5260@gmail.com>
2023-04-09 13:13:35 -0400

    Added Segment class, it's instance variables, and constructor

---
+++ src/Segment.java
@@ -0,0 +1,12 @@
+public class Segment {
+
+    private int x;
+    private int y;
+    private boolean head;
+
+    public Segment(int y, int x, boolean head) {
+        this.x = x;
+        this.y = y;
+        this.head = head;
+    }
+}
