3d93e1b75e916b3693c06cbd0b8e93ac37804871
Rogers <brogers5260@gmail.com>
2023-04-09 12:57:05 -0400

    Added Snake class and instantiated an instance of Snake in Game class

--- src/Game.java
+++ src/Game.java
@@ -3,10 +3,12 @@ import java.awt.*;
 
 public class Game {
     public static void main(String[] args) {
+        Snake snake = new Snake();
         JFrame frame = new JFrame();
         JPanel gamePanel = new JPanel();
         gamePanel.setPreferredSize(new Dimension(720, 720));
         gamePanel.setBackground(Color.black);
+        gamePanel.add(snake);
 
         frame.add(gamePanel);
         frame.pack();
