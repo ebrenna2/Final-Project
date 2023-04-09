3cd81bf9f872a533eceede0eee7a3321cf1e21b9
Rogers <brogers5260@gmail.com>
2023-04-09 13:16:44 -0400

    Added instance variables and constructor to Snake

--- src/Snake.java
+++ src/Snake.java
@@ -1,5 +1,14 @@
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class Snake extends JComponent {
+
+    ArrayList<Segment> body;
+    int score;
+
+    Snake() {
+        body = new ArrayList<>();
+        score = 0;
+    }
 
 }
