bd949c777eb488ba0b29524ea894a675a1f0a454
Rogers <brogers5260@gmail.com>
2023-04-10 00:48:59 -0400

    Implemented KeyListener interface in Snake

--- src/Snake.java
+++ src/Snake.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
-public class Snake extends JComponent {
+public class Snake extends JComponent implements KeyListener {
 
     private ArrayList<Segment> body;
     private int score;
@@ -22,5 +24,20 @@ public class Snake extends JComponent {
         for (Segment segment: body) {
             segment.draw(g);
         }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
