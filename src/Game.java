31448eb81269356eb6b9e5a74700e0123fcbe4c8
landers7UMW <landers7@mail.umw.edu>
2023-04-13 16:01:55 -0400

    Creating gridSize (for Dimension) and ArrayList<ArrayList<State>> grid for 2D array and createGrid method

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,19 @@
+package src;
+
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class Game {
 
     private static boolean lose = false;
+    static int gridSize=36;
+    static ArrayList<ArrayList<State>> grid=createGrid(gridSize);
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         gameBoard board = new gameBoard();
         frame.getContentPane().add(board);
-        frame.setSize(720, 720);
+        frame.setSize(gridSize*20, gridSize*20);
 
         frame.pack();
         frame.setVisible(true);
@@ -22,5 +27,25 @@ public class Game {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    public static ArrayList<ArrayList<State>> createGrid(int gridSize) {
+        ArrayList<ArrayList<State>> grid=new ArrayList<>();
+        ArrayList<State> wallRow=new ArrayList<>();
+        for (int i=0;i<gridSize;i++) {
+            wallRow.add(State.WALL);
+        }
+        ArrayList<State> emptyRow=new ArrayList<>();
+        emptyRow.add(State.WALL);
+        for (int i=0;i<gridSize;i++) {
+            emptyRow.add(State.EMPTY);
+        }
+        emptyRow.add(State.WALL);
+        grid.add(wallRow);
+        for (int i=0;i<gridSize;i++) {
+            grid.add(emptyRow);
+        }
+        grid.add(wallRow);
+        return grid;
     }
 }
