334180fa870e96c6b60a4ad9ee52c37bdbdca127
Rogers <brogers5260@gmail.com>
2023-04-15 00:59:47 -0400

    Added the grid and gridSize methods made by Lucas as well as his createGrid method. Added a method called checkLoseCondition which contains his code checking collision.

--- src/gameBoard.java
+++ src/gameBoard.java
@@ -1,9 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class gameBoard extends JPanel {
 
     private Snake snake;
+    private static int gridSize=36;
+    static ArrayList<ArrayList<State>> grid=createGrid(gridSize);
 
     gameBoard() {
         snake = new Snake();
@@ -15,6 +18,38 @@ public class gameBoard extends JPanel {
         setFocusable(true);
     }
 
+    public void move() {
+        snake.move();
+    }
+
+    public boolean checkLoseCondition() {
+        Segment head = snake.getBody().get(0);
+        if (head.getX() < 0 || head.getY() < 0) return true;
+        if (grid.get(head.getY()/20).get(head.getX()/20)== State.WALL||
+                grid.get(head.getY()/20).get(head.getX()/20)==State.BODY) {
+            return true;
+        } else return false;
+    }
+
+    public static ArrayList<ArrayList<State>> createGrid(int gridSize) {
+        ArrayList<ArrayList<State>> grid=new ArrayList<>();
+        ArrayList<State> wallRow=new ArrayList<>();
+        for (int i=0;i<gridSize;i++) {
+            wallRow.add(State.WALL);
+        }
+        ArrayList<State> emptyRow=new ArrayList<>();
+        emptyRow.add(State.WALL);
+        for (int i=0;i<gridSize;i++) {
+            emptyRow.add(State.EMPTY);
+        }
+        emptyRow.add(State.WALL);
+        grid.add(wallRow);
+        for (int i=0;i<gridSize;i++) {
+            grid.add(emptyRow);
+        }
+        grid.add(wallRow);
+        return grid;
+    }
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
