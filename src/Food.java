5eae55be53d1d40f644ca1ceb5d758f5e1c25d44
Rogers <brogers5260@gmail.com>
2023-04-20 01:30:11 -0400

    Removed unused code

--- src/Food.java
+++ src/Food.java
@@ -6,14 +6,6 @@ public class Food {
     public void paintCookie(Graphics g, int x, int y ) {
         g.setColor(new Color(173, 93, 41)); // set color of cookie
         g.fillOval(x, y, 20, 20); // draw cookie shape
-        g.setColor(new Color(120, 63, 4)); // set color of chocolate chips
-        int numChips = 10; // number of chocolate chips on cookie
-//        for (int i=0; i < numChips; i++) {
-//            int chipX = x + (int)(Math.random() * 20);
-//            int chipY = y + (int)(Math.random() * 20);
-//            int chipSize = (int)(20 * 0.1);
-//            g.fillOval(chipX, chipY, chipSize, chipSize);
-//        }
     }
 
     Food() {
